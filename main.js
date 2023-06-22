36b55a9bf5b2f19e45e5f6310e4d3880d0fe0b47
Bonello <geno.bonello@gmail.com>
2023-06-21 17:36:43 -0700

    added delete button and word wrap

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 const txtArea= document.getElementById("text-area")
 const sbmtBtn = document.getElementById("note-submit-btn")
 const notesDiv = document.getElementById("notes-div")
+const noteSnippit = document.getElementsByClassName("note-snippet")
+const localNoteStorage = JSON.parse(localStorage.getItem("notes"))
 let notesArray = []
+
+
 
 sbmtBtn.addEventListener("click", function(){
     notesArray.push(txtArea.value)
+    localStorage.setItem("notes", JSON.stringify(notesArray))
     renderNotes()
 })
 
 const renderNotes = () => {
     notesDiv.innerHTML = ""
     for (let i = 0; i<notesArray.length; i++){
-        notesDiv.innerHTML += `<p class="note-snippet">${notesArray[i]}</p><button>Hello</button>`
+        notesDiv.innerHTML += `<p class="note-snippet">${notesArray[i]}</p><button class="delete-button" onclick="remove(${i})">Delete</button>`
     }
+    txtArea.value = ""
+}
+
+if(localNoteStorage){
+    notesArray = localNoteStorage
+    renderNotes()
+}
+
+function remove(index){
+    notesArray.splice(index, 1)
+    localStorage.setItem("notes", JSON.stringify(notesArray))
+    renderNotes()
 }
